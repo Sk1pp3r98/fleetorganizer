627fe636def987fbc576c167e341a10095e0ee6b
Sk1pp3r98 <johannes.ochtrup@gmail.com>
2026-02-18 01:40:23 +0100

    random shit und tourmanager

---
+++ app/components/table/base/Header.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <header>
+    <div class="container mx-auto p-4">
+    Header
+    </div>
+  </header>
+</template>
+
+<style>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   },
   components: [
     {
-      path: '~/components', // will get any components nested in let's say /components/test too
+      path: '~/components',
       pathPrefix: false,
     },
   ]
